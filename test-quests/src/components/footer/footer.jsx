45391cc000f3a089c62aca385ad5de7459807c15
Giacomo <giacomo.ratto00@gmail.com>
2023-11-27 14:31:28 +0100

    add placeholder for header and footer

---
+++ test-quests/src/components/footer/footer.jsx
@@ -0,0 +1,9 @@
+import './footer.css'
+
+function Footer() {
+	return (
+		<footer>This is a footer</footer>
+	)
+}
+
+export default Footer
---
+++ test-quests/src/components/header/header.jsx
@@ -0,0 +1,9 @@
+import './header.css'
+
+function Header() {
+	return (
+		<header>This is a header</header>
+	)
+}
+
+export default Header
