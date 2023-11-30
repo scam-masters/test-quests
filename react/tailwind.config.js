a3c698ee1848fa168be41f89699ee03172156e02
Giorgio Rengucci <37120983+GiorgioRen@users.noreply.github.com>
2023-11-30 22:02:15 +0100

    Update colors for dark mode

--- react/tailwind.config.js
+++ react/tailwind.config.js
@@ -6,12 +6,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'tq-primary': '#374161',
-        'tq-accent':  '#3C486B',
-        'tq-red':     '#F45050',
-        'tq-yellow':  '#F9D949',
-        'tq-green':   '#69995D',
-        'tq-white':   '#F0F0F0',
+        'tq-primary': 'var(--color-primary)',
+        'tq-accent':  'var(--color-accent)',
+        'tq-red':     'var(--color-red)',
+        'tq-yellow':  'var(--color-yellow)',
+        'tq-green':   'var(--color-green)',
+        'tq-white':   'var(--color-white)',
+        'tq-black':   'var(--color-black)',
       },
       fontFamily: {
         'logo': ['Lexend Deca', 'sans-serif'],
