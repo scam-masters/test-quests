c6a6fc0d17cf45c28a949f40f0c10d2d7beb0479
Giacomo Pedemonte <giacomopedemonte@libero.it>
2023-12-03 17:22:59 +0100

    add learning page and merge with drag components

--- react/src/App.js
+++ react/src/App.js
@@ -1,37 +1,34 @@
-import React, { useState } from 'react';
-import Header from './components/header/header';
-import Footer from './components/footer/footer';
-import Landing from './pages/Landing';
-import M1Learning from './pages/M1Learning';
-import M1Exercise from './pages/M1Exercise';
-import NotfoundERR from './pages/NotfoundERR';import DndExercise from './pages/DragAndDropExercise';
-
+import React, { useState } from "react";
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
+import Landing from "./pages/Landing";
+import M1Learning from "./pages/M1Learning";
+import M1Exercise from "./pages/M1Exercise";
+import NotfoundERR from "./pages/NotfoundERR";
+import DndExercise from "./pages/DragAndDropExercise";
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('landing');
+  const [currentPage, setCurrentPage] = useState("landing");
 
   const switchPage = (page) => {
     setCurrentPage(page);
   };
 
   return (
-    <div className='bg-tq-white dark:bg-tq-black h-screen w-screen overflow-y-auto scrollbar-hide'>
-      <Header  />
-      {
-				currentPage === 'landing' ? (
-		        <Landing switchPage={switchPage} />
-      ) : currentPage === 'm1learning' ? (
+    <div className="bg-tq-white dark:bg-tq-black h-screen w-screen overflow-y-auto scrollbar-hide">
+      <Header />
+      {currentPage === "landing" ? (
+        <Landing switchPage={switchPage} />
+      ) : currentPage === "m1learning" ? (
         <M1Learning switchPage={switchPage} />
-      ) : currentPage === 'm1exercise' ? (
+      ) : currentPage === "m1exercise" ? (
         <M1Exercise switchPage={switchPage} />
-	      ) : 
-				currentPage === 'exercise' ? (
-					<DndExercise switchPage={switchPage} />
-				) :
-				(
-			<NotfoundERR switchPage={switchPage}/>
-		  )}
-      <Footer  />
+      ) : currentPage === "exercise" ? (
+        <DndExercise switchPage={switchPage} />
+      ) : (
+        <NotfoundERR switchPage={switchPage} />
+      )}
+      <Footer />
     </div>
   );
 }
