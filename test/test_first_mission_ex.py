12f9d5e5d9aa0a37ba7e76b362df0001b3d82b5a
LoScarro <davisca2000@gmail.com>
2023-12-08 10:13:25 +0100

    implemented drag and drop tests

---
+++ test/test_first_mission_ex.py
@@ -0,0 +1,61 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+base_url = "http://localhost:3000/"
+expected_title = "React App"
+
+driver = webdriver.Chrome()
+
+try:
+    driver.get(base_url)
+    WebDriverWait(driver, 10).until(EC.title_contains(expected_title)) # Wait for the page to load
+
+    '''
+    Click on the first mission button
+    '''
+    driver.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div[1]/button').click()
+
+    '''
+    Click on "Start Exercise" button
+    '''
+
+    driver.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div/div[2]/button[2]').click()
+
+    '''
+    Drag and drop the first element
+    '''
+    wait = WebDriverWait(driver, 10)
+
+
+    element = driver.find_element(By.XPATH, '//*[@id="pane2_1"]/div/div/span[1]')
+    target = driver.find_element(By.XPATH, '//*[@id="pane2_1"]/div/p[2]/span[1]')
+
+    from selenium.webdriver import ActionChains
+    action_chains = ActionChains(driver)
+    action_chains.drag_and_drop(element, target).perform()
+
+    wait = WebDriverWait(driver, 10000)
+
+    '''
+    Assert that the first element has been moved
+    '''
+
+    assert driver.find_element(By.XPATH, '//*[@id="pane2_1"]/div/p[2]/span[1]').text == "../"
+
+    # '''
+    # Test 2: Check the first CircleMission is present and has the right text 
+    # '''
+    # first_circle_mission = driver.find_element(By.XPATH, "/html/body/div/div/div[2]/div[1]/button")
+    # assert first_circle_mission.text == "Path Traversal"
+    
+    # '''
+    # Test 3: Check for accessing to the first learning page
+    # '''
+    # first_circle_mission.click()
+    # assert driver.find_element(By.XPATH, "/html/body/div/div/button").text == "Go back to main page"
+
+finally:
+    print("All the tests are successful!")
+    driver.quit()
