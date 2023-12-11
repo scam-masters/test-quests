760abd535216eb7fd42eb4dab58735a1a8c39a6e
LoScarro <davisca2000@gmail.com>
2023-12-11 22:44:44 +0100

    Update test for testing score

--- test/test_score.py
+++ test/test_score.py
@@ -1,149 +1,134 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver import ActionChains
 
-base_url = "http://localhost:3001/"
+base_url = "http://localhost:3000/"
 expected_title = "React App"
 
 driver = webdriver.Chrome()
 
-try:
+
+def goto_first_mission():
     driver.get(base_url)
-    WebDriverWait(driver, 10).until(EC.title_contains(expected_title))  # Wait for the page to load
-
-    # Click on the first mission button
+    '''
+    Wait for the page to load
+    '''
+    WebDriverWait(driver, 10).until(EC.title_contains(expected_title))
+    '''
+    Click on the first mission button
+    '''
     driver.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div[1]/button').click()
-
-    # Click on "Start Exercise" button
+    '''
+    Click on "Start Exercise" button
+    '''
     driver.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div/div[2]/button[2]').click()
 
-    # Drag and drop the first element (assuming you have the necessary code here)
+try:
+    goto_first_mission()
+    '''
+    Get all the elements and targets
+    '''
+    target1 = driver.find_element(By.XPATH, '//*[@id="pane2_1"]/div/p[2]/span[1]')
+    target2 = driver.find_element(By.XPATH, '//*[@id="pane2_1"]/div/p[2]/span[2]')
+    target3 = driver.find_element(By.XPATH, '//*[@id="pane2_1"]/div/p[2]/span[3]')
+    target4 = driver.find_element(By.XPATH, '//*[@id="pane2_1"]/div/p[2]/span[4]')
+    target5 = driver.find_element(By.XPATH, '//*[@id="pane2_1"]/div/p[2]/span[5]')
+    target6 = driver.find_element(By.XPATH, '//*[@id="pane2_1"]/div/p[2]/span[6]')
+    target7 = driver.find_element(By.XPATH, '//*[@id="pane2_1"]/div/p[2]/span[7]')
 
-    # Click on the submit button
-    submit_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//button[@class='bg-tq-red hover:bg-tq-red-500 text-tq-white font-bold py-2 px-4 rounded-full transition duration-200 ease-in-out hover:shadow-lg hover:scale-105 active:scale-95 active:shadow-md']"))
-    )
-    submit_button.click()
-
-    # Wait for the popup to be displayed
-    WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located((By.XPATH, "/html[1]/body[1]/div[2]/div[1]"))
-    )
-
-    # Assert that the popup is shown
-    assert driver.find_element(By.XPATH, "/html[1]/body[1]/div[2]/div[1]").is_displayed(), "Popup not displayed"
-    
-    # now I need to close the popup and try to check the different messages that the user can recieve depending on the score he gets
-    popup_button = "/html[1]/body[1]/div[2]/div[1]/div[2]/div[1]/div[2]/button[1]"
-    
-    driver.find_element(By.XPATH, popup_button).click()
-    
-    # try to make swaps to check the score correctness
-    target1 = '//*[@id="pane2_1"]/div/p[2]/span[1]'
-    target2 = '//*[@id="pane2_1"]/div/p[2]/span[2]'
-    target3 = '//*[@id="pane2_1"]/div/p[2]/span[3]'
-    target4 = '//*[@id="pane2_1"]/div/p[2]/span[4]'
-    target5 = '//*[@id="pane2_1"]/div/p[2]/span[5]'
-    target6 = '//*[@id="pane2_1"]/div/p[2]/span[6]'
-    target7 = '//*[@id="pane2_1"]/div/p[2]/span[7]'
-    
-    tar1 = driver.find_element(By.XPATH, target1)
-    tar2 = driver.find_element(By.XPATH, target2)
-    tar3 = driver.find_element(By.XPATH, target3)
-    tar4 = driver.find_element(By.XPATH, target4)
-    tar5 = driver.find_element(By.XPATH, target5)
-    tar6 = driver.find_element(By.XPATH, target6)
-    tar7 = driver.find_element(By.XPATH, target7)
-    
-    starting1 = '//*[@id="pane2_1"]/div/div/span[1]'
-    starting2 = '//*[@id="pane2_1"]/div/div/span[2]'
-    starting3 = '//*[@id="pane2_1"]/div/div/span[3]'
-    starting4 = '//*[@id="pane2_1"]/div/div/span[4]'
-    starting5 = '//*[@id="pane2_1"]/div/div/span[5]'
-    starting6 = '//*[@id="pane2_1"]/div/div/span[6]'
-    starting7 = '//*[@id="pane2_1"]/div/div/span[7]'
-    
-    star1 = driver.find_element(By.XPATH, starting1)
-    star2 = driver.find_element(By.XPATH, starting2)
-    star3 = driver.find_element(By.XPATH, starting3)
-    star4 = driver.find_element(By.XPATH, starting4)
-    star5 = driver.find_element(By.XPATH, starting5)
-    star6 = driver.find_element(By.XPATH, starting6)
-    star7 = driver.find_element(By.XPATH, starting7)
-    
-    from selenium.webdriver import ActionChains
+    element1 = driver.find_element(By.XPATH, '//*[@id="pane2_1"]/div/div/span[1]')
+    element2 = driver.find_element(By.XPATH, '//*[@id="pane2_1"]/div/div/span[2]')
+    element3 = driver.find_element(By.XPATH, '//*[@id="pane2_1"]/div/div/span[3]')
+    element4 = driver.find_element(By.XPATH, '//*[@id="pane2_1"]/div/div/span[4]')
+    element5 = driver.find_element(By.XPATH, '//*[@id="pane2_1"]/div/div/span[5]')
+    element6 = driver.find_element(By.XPATH, '//*[@id="pane2_1"]/div/div/span[6]')
+    element7 = driver.find_element(By.XPATH, '//*[@id="pane2_1"]/div/div/span[7]')
+    '''
+    Get the button for submitting the exercise
+    '''
+    submit_button = driver.find_element(By.XPATH, '//*[@id="root"]/div/div[3]/button[2]')
+    '''
+    Popup button 
+    '''
+    popup_button = '//*[@id="pr_id_6_content"]/div/div[2]/button'
+    '''
+    Case 1: everything placed correctly (if you submit and click the popup button you will return to page)
+    '''
     action_chains = ActionChains(driver)
-    # case1: nothing placed correctly or not all the blanks filled (if you submit and click the popup button you remain in the same exercise page)
-    action_chains.drag_and_drop(star1, tar1).perform()
-    action_chains.drag_and_drop(star2, tar2).perform()
-    action_chains.drag_and_drop(star3, tar3).perform()
-    action_chains.drag_and_drop(star4, tar4).perform()
-    action_chains.drag_and_drop(star5, tar5).perform()
-    action_chains.drag_and_drop(star6, tar6).perform()
-    action_chains.drag_and_drop(star7, tar7).perform()
-    
-    wait = WebDriverWait(driver, 1000)
+    action_chains.drag_and_drop(element1, target2).perform()
+    action_chains.drag_and_drop(element2, target4).perform()
+    action_chains.drag_and_drop(element3, target7).perform()
+    action_chains.drag_and_drop(element4, target3).perform()
+    action_chains.drag_and_drop(element5, target5).perform()
+    action_chains.drag_and_drop(element6, target1).perform()
+    action_chains.drag_and_drop(element7, target6).perform()
+    '''
+    Submit the exercise
+    '''
+    submit_button.click()
+    '''
+    Press the button on the popup
+    '''
+    driver.find_element(By.XPATH, popup_button).click()
+    '''
+    We should be in the home page
+    '''
+    assert driver.find_element(By.XPATH, '//*[@id="root"]/div/div[2]/div[1]/button').is_displayed(), "You are still in the exercise page"
     
     '''
-    Assert that the first element has been moved
+    Return to the exercise page
     '''
-    assert driver.find_element(By.XPATH, target1).text == "?", "Not performed the swap"
-    
-    # Click on the submit button
-    submit_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//button[@class='bg-tq-red hover:bg-tq-red-500 text-tq-white font-bold py-2 px-4 rounded-full transition duration-200 ease-in-out hover:shadow-lg hover:scale-105 active:scale-95 active:shadow-md']"))
-    )
+    goto_first_mission()
+    '''
+    Get again all the elements and targets
+    '''
+    target1 = driver.find_element(By.XPATH, '//*[@id="pane2_1"]/div/p[2]/span[1]')
+    target2 = driver.find_element(By.XPATH, '//*[@id="pane2_1"]/div/p[2]/span[2]')
+    target3 = driver.find_element(By.XPATH, '//*[@id="pane2_1"]/div/p[2]/span[3]')
+    target4 = driver.find_element(By.XPATH, '//*[@id="pane2_1"]/div/p[2]/span[4]')
+    target5 = driver.find_element(By.XPATH, '//*[@id="pane2_1"]/div/p[2]/span[5]')
+    target6 = driver.find_element(By.XPATH, '//*[@id="pane2_1"]/div/p[2]/span[6]')
+    target7 = driver.find_element(By.XPATH, '//*[@id="pane2_1"]/div/p[2]/span[7]')
+
+    element1 = driver.find_element(By.XPATH, '//*[@id="pane2_1"]/div/div/span[1]')
+    element2 = driver.find_element(By.XPATH, '//*[@id="pane2_1"]/div/div/span[2]')
+    element3 = driver.find_element(By.XPATH, '//*[@id="pane2_1"]/div/div/span[3]')
+    element4 = driver.find_element(By.XPATH, '//*[@id="pane2_1"]/div/div/span[4]')
+    element5 = driver.find_element(By.XPATH, '//*[@id="pane2_1"]/div/div/span[5]')
+    element6 = driver.find_element(By.XPATH, '//*[@id="pane2_1"]/div/div/span[6]')
+    element7 = driver.find_element(By.XPATH, '//*[@id="pane2_1"]/div/div/span[7]')
+    '''
+    Get the button for submitting the exercise
+    '''
+    submit_button = driver.find_element(By.XPATH, '//*[@id="root"]/div/div[3]/button[2]')
+    '''
+    Case 2: nothing placed correctly or not all the blanks filled (if you submit and click the popup button you remain in the same exercise page)
+    '''
+    #action_chains = ActionChains(driver)
+    action_chains.drag_and_drop(element1, target4).perform()
+    action_chains.drag_and_drop(element2, target2).perform()
+    action_chains.drag_and_drop(element3, target3).perform()
+    action_chains.drag_and_drop(element4, target7).perform()
+    action_chains.drag_and_drop(element5, target1).perform()
+    action_chains.drag_and_drop(element6, target6).perform()
+    action_chains.drag_and_drop(element7, target5).perform()
+    '''
+    Submit the exercise
+    '''
     submit_button.click()
-
-    # Wait for the popup to be displayed
-    WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located((By.XPATH, "/html[1]/body[1]/div[2]/div[1]"))
-    )
-
-    # Assert that the popup is shown
-    assert driver.find_element(By.XPATH, "/html[1]/body[1]/div[2]/div[1]").is_displayed(), "Popup not displayed"
-
-    # click on the button on the popup    
+    '''
+    Press the button on the popup
+    '''
     driver.find_element(By.XPATH, popup_button).click()
+    '''
+    We should be in the home page
+    '''
+    assert element1.is_displayed(), "You are no more in the exercise page"
+    #time.sleep(100)
     
-    # Assert you are always in the same exercise page, only when the submission is correct you can click and go back to main page
-    assert driver.find_element(By.XPATH, target1).is_displayed(), "Maybe you are not in the same page"
-    
-    # case2: all the swaps performed properly, then the click on the submission button will make me obtain the redirection to the home page from the button of the popup
-    driver.refresh()
-    
-    wait = WebDriverWait(driver, 10000)
-    
-    action_chains.drag_and_drop(star1, tar2).perform()
-    action_chains.drag_and_drop(star2, tar4).perform()
-    action_chains.drag_and_drop(star3, tar7).perform()
-    action_chains.drag_and_drop(star4, tar3).perform()
-    action_chains.drag_and_drop(star5, tar5).perform()
-    action_chains.drag_and_drop(star6, tar1).perform()
-    action_chains.drag_and_drop(star7, tar6).perform()
-    
-    # Click on the submit button
-    submit_button = WebDriverWait(driver, 10).until(
-        EC.element_to_be_clickable((By.XPATH, "//button[@class='bg-tq-red hover:bg-tq-red-500 text-tq-white font-bold py-2 px-4 rounded-full transition duration-200 ease-in-out hover:shadow-lg hover:scale-105 active:scale-95 active:shadow-md']"))
-    )
-    submit_button.click()
-
-    # Wait for the popup to be displayed
-    WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located((By.XPATH, "/html[1]/body[1]/div[2]/div[1]"))
-    )
-
-    # Assert that the popup is shown
-    assert driver.find_element(By.XPATH, "/html[1]/body[1]/div[2]/div[1]").is_displayed(), "Popup not displayed"
-    
-    # click on the button on the popup
-    driver.find_element(By.XPATH, popup_button).click()
-    
-    path_traversal = '//*[@id="root"]/div/div[2]/div[1]/button'
-    
-    # Assert you are always in the same exercise page, only when the submission is correct you can click and go back to main page
-    assert driver.find_element(By.XPATH, path_traversal).is_displayed(), "You should be in the landing page"
     
 finally:
     print("All the tests are successful!")
