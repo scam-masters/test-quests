e69ac93266fa723e460b0100a2088fc46b7fa2da
LLaCorte <lorenzolacorte99@gmail.com>
2023-12-13 11:29:10 +0100

    Fix Burndown Chart (with Hours)

--- scrum/burndown_chart.py
+++ scrum/burndown_chart.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
 import argparse
+from collections import defaultdict
 
 parser = argparse.ArgumentParser(description='Build a Burndown Chart from a Notion Database')
 load_dotenv()
@@ -58,57 +59,72 @@ def get_tasks():
 def create_burndown(tasks, chosen_sprint):
     data = []
 
-    total_tasks = 0
+    sprint_tasks = 0
+    sprint_hours = 0
+    hours_spent_per_day = defaultdict(int) 
 
     for page in tasks:
         props = page["properties"]
         
-        sprint = props.get("Sprint", {}).get("select", {})
-        if sprint:
-            sprint = sprint.get("name", "").lower()
-            total_tasks += 1
+        try:
+            sprint = props.get("Sprint", {}).get("select", {}).get("name", "").lower()
         
-        if sprint == chosen_sprint.lower():
-            status = props.get("Status", {}).get("status", {}).get("name", "").lower()
-            if status == "done":
-                completion_date = props.get("Completion Date", {}).get("date", {}).get("start", "")
-                completion_date = datetime.fromisoformat(completion_date)
-                data.append({"Page ID": page["id"], "Date": completion_date})
-    
+            if sprint == chosen_sprint.lower() and props.get("Estimate", {}): 
+                task_estimate_hours = float(props.get("Estimate", {}).get("select", {}).get("name", "").lower().split("[")[1].split("]")[0])
+                sprint_tasks += 1
+                sprint_hours += task_estimate_hours
+                status = props.get("Status", {}).get("status", {}).get("name", "").lower()
+        
+                if status == "done":
+                    completion_date = props.get("Completion Date", {}).get("date", {}).get("start", "")
+                    completion_date = datetime.fromisoformat(completion_date)
+
+                    hours_spent_per_day[completion_date] += task_estimate_hours
+
+                    data.append({"Page ID": page["id"], "Date": completion_date})
+        
+        except AttributeError:
+            continue
+
     df = pd.DataFrame(data)
+    if df.empty: return
     df["Date"] = df["Date"].dt.date
 
     all_days_in_sprint = pd.date_range(start=df["Date"].min(), end=df["Date"].max(), freq="D")
     all_days_df = pd.DataFrame({"Date": all_days_in_sprint})
     all_days_df["Date"] = all_days_df["Date"].dt.date
 
-    tasks_done_per_day = df.groupby("Date").size().reset_index(name="T_Done")
-    tasks_done_per_day = pd.merge(all_days_df, tasks_done_per_day, on="Date", how="left").fillna(0)
-    print(tasks_done_per_day)
+    hours_spent_per_day_df = pd.DataFrame.from_dict(hours_spent_per_day, orient="index", columns=['Hours'])
+    hours_spent_per_day_df = hours_spent_per_day_df.reset_index().rename(columns={'index': 'Date'})
+    print(hours_spent_per_day_df)
 
-    tasks_remaining_per_day = pd.DataFrame({
+    hours_spent_per_day_df["Date"] = hours_spent_per_day_df["Date"].dt.date
+    hours_spent_per_day_df = pd.merge(all_days_df, hours_spent_per_day_df, on="Date", how="left").fillna(0).groupby('Date')['Hours'].sum().reset_index()
+    print(hours_spent_per_day_df)
+
+    hours_remaining_per_day = pd.DataFrame({
         "Date": all_days_in_sprint,
-        "T_Remaining": total_tasks - tasks_done_per_day["T_Done"].cumsum()
+        "Hours_Remaining": sprint_hours - hours_spent_per_day_df["Hours"].cumsum()
     })
 
-    ideal_tasks_per_day = total_tasks / len(all_days_in_sprint)
-    tasks_remaining_per_day["Ideal"] = total_tasks - ideal_tasks_per_day * tasks_remaining_per_day.index
+    ideal_hours_per_day = sprint_hours / len(all_days_in_sprint)
+    hours_remaining_per_day["Ideal"] = sprint_hours - ideal_hours_per_day * hours_spent_per_day_df.index
 
     plt.figure(figsize=(10, 6))
     
-    plt.plot(tasks_remaining_per_day["Date"], tasks_remaining_per_day["T_Remaining"], label="Remaining Tasks", marker='o')
-    plt.plot(tasks_remaining_per_day["Date"], tasks_remaining_per_day["Ideal"], label="Ideal", linestyle='--')
+    plt.plot(hours_remaining_per_day["Date"], hours_remaining_per_day["Hours_Remaining"], label="Remaining Hours", marker='o')
+    plt.plot(hours_remaining_per_day["Date"], hours_remaining_per_day["Ideal"], label="Ideal", linestyle='--')
     
     plt.xlabel("Date")
-    plt.ylabel("Number of Tasks")
-    plt.title("Burndown Chart")
+    plt.ylabel("Number of Hours")
+    plt.title(f"Burndown Chart - {sprint}")
     plt.legend()
     plt.xticks(rotation=45)
     plt.tight_layout()
-    if os.environ["NOTION_TOKEN"]:
-        plt.savefig(f"scrum/{sprint}/burndown_chart.png")
-    else:
-        plt.savefig(f"{sprint}/burndown_chart.png")
+
+    cwd = os.path.dirname(os.path.realpath(__file__))
+    date = datetime.today().strftime('%Y-%m-%d')    
+    plt.savefig(f"{cwd}/{sprint}/bd-{date}.png")
 
 
 if __name__ == "__main__":
@@ -117,4 +133,4 @@ if __name__ == "__main__":
 
     sprint = f"sprint {args.sprint_num}"
     tasks = get_tasks()
-    create_burndown(tasks, sprint)
+    create_burndown(tasks, "sprint 1")
