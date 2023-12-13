6860c0db067eef22a87251db2c5394f566613778
Giorgio Rengucci <37120983+GiorgioRen@users.noreply.github.com>
2023-12-13 16:50:05 +0100

    go to home from header

--- next-app/src/components/header/header.jsx
+++ next-app/src/components/header/header.jsx
@@ -1,9 +1,13 @@
+import Link from "next/link"
+
 function Header() {
 	return (
 		<>
 			<header className="fixed w-full flex flex-col justify-center items-center bg-tq-primary z-50">
-				<img alt="logo" src="/assets/logo-transparent.svg" className="h-24" />
-				<h1 className='font-logo text-tq-white h-0 -translate-y-11'>TEST QUESTS</h1>
+				<Link href="/">
+					<img alt="logo" src="/assets/logo-transparent.svg" className="h-24" />
+					<h1 className='font-logo text-tq-white h-0 -translate-y-11'>TEST QUESTS</h1>
+				</Link>
 			</header>
 			<div className={`w-full pt-24`}></div>
 		</>
