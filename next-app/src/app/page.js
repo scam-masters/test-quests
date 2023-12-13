19cf5229bbc76dd79aba2439c40b77c1a0b60f8a
Giacomo <giacomo.ratto00@gmail.com>
2023-12-13 16:58:40 +0100

    add header and footer in every page

--- next-app/src/app/page.js
+++ next-app/src/app/page.js
@@ -1,13 +1,7 @@
-import Header from '@/components/header/header'
 import Landing from './pages/Landing'
-import Footer from '@/components/footer/footer'
 
 export default function Home() {
-  return (
-    <>
-      <Header />
-      <Landing />
-      <Footer />
-    </>
-  )
+	return (
+		<Landing />
+	)
 }
