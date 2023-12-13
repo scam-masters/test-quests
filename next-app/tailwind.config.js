3bfec6d038bcb5a6b6333a4bab017355013ac7ef
Giorgio Rengucci <37120983+GiorgioRen@users.noreply.github.com>
2023-12-13 15:46:04 +0100

    update tailwind config

--- next-app/tailwind.config.js
+++ next-app/tailwind.config.js
@@ -7,11 +7,19 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'tq-primary': 'var(--color-primary)',
+        'tq-accent': 'var(--color-accent)',
+        'tq-gray': 'var(--color-gray)',
+        'tq-red': 'var(--color-red)',
+        'tq-yellow': 'var(--color-yellow)',
+        'tq-green': 'var(--color-green)',
+        'tq-white': 'var(--color-white)',
+        'tq-black': 'var(--color-black)',
       },
+      fontFamily: {
+        'logo': ['Lexend Deca', 'sans-serif'],
+      }
     },
   },
   plugins: [],
