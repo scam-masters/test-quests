17285a88af0154acf87b3f9b6a84aa1e908a7d2b
Giorgio Rengucci <37120983+GiorgioRen@users.noreply.github.com>
2023-12-13 16:05:03 +0100

    fix logo

--- next-app/src/app/page.js
+++ next-app/src/app/page.js
@@ -1,7 +1,13 @@
+import Header from '@/components/header/header'
 import Landing from './pages/Landing'
+import Footer from '@/components/footer/footer'
 
 export default function Home() {
   return (
-    <Landing />
+    <>
+      <Header />
+      <Landing />
+      <Footer />
+    </>
   )
 }
--- next-app/src/app/pages/Landing.jsx
+++ next-app/src/app/pages/Landing.jsx
@@ -1,3 +1,5 @@
+"use client"
+import React, { useState } from 'react';
 import CircleMission from '@/components/button/circle_mission';
 import { Dialog } from 'primereact/dialog';
 
@@ -19,7 +21,7 @@ function Landing({ switchPage }) {
     return (
         <div className='p-10'>
             <div className='text-center relative align-middle'>
-                <CircleMission onClick={() => switchPage('m1learning')} type="gradient">Path Traversal</CircleMission>
+                <CircleMission type="gradient">Path Traversal</CircleMission>
             </div>
             <div className='text-center mt-5 relative right-14 align-middle' onClick={MissionLocked}>
                 <CircleMission type="locked">Mission Locked</CircleMission>
