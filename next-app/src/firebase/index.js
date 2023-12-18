86653f9e950d861b879c0a76c42698ee6b6358fe
LLaCorte <lorenzolacorte99@gmail.com>
2023-12-18 17:59:17 +0100

    Export Database

--- next-app/src/firebase/index.js
+++ next-app/src/firebase/index.js
@@ -1,9 +1,10 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Import the functions you need from the SDKs you need
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAx-n-RRkzYIfIJUoKnToix3E8OEfnkkqI",
   authDomain: "test-quests-a3712.firebaseapp.com",
@@ -13,6 +14,8 @@ const firebaseConfig = {
   appId: "1:249502392786:web:6086acbbb9986197f5a80b"
 };
 
-// Initialize Firebase
+// Initialize Firebase: https://firebase.google.com/docs/firestore/quickstart
 const app = initializeApp(firebaseConfig);
-export default app;
+const db = getFirestore(app);
+
+export default db;
