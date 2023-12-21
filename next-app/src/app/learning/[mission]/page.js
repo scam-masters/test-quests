9e0b9c04082f079da49c9d3c8e6fe5b7ae9306c5
Giacomo <giacomo.ratto00@gmail.com>
2023-12-21 18:46:17 +0100

    use dynamic routing

--- next-app/src/app/learning/[mission]/page.js
+++ next-app/src/app/learning/[mission]/page.js
@@ -4,10 +4,11 @@ import LearningComponent from '@/components/Learning/learning'
 
 import { getExerciseData } from "@/app/actions"
 
-// M1Learning Component
-async function M1Learning() {
-	// Get the content for Mission 1
-	const missionContent = await getExerciseData("mission_1");
+// Learning Component
+// https://nextjs.org/docs/app/building-your-application/routing/dynamic-routes
+export default async function Learning({ params }) {
+	// Get the current mission
+	const missionContent = await getExerciseData(`mission_${params.mission}`);
 
 	return (
 		<div>
@@ -17,4 +18,3 @@ async function M1Learning() {
 	);
 }
 
-export default M1Learning;
