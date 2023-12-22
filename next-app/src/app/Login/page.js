92b11603ab2ea084ebfec36555267dc74d05367f
Giacomo <giacomo.ratto00@gmail.com>
2023-12-22 15:51:55 +0100

    error handling in login

--- next-app/src/app/Login/page.js
+++ next-app/src/app/Login/page.js
@@ -1,74 +1,81 @@
 "use client";
+
 // Import necessary packages
 import React, { useState } from "react";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { app } from "@/firebase/index";
 import Link from "next/link";
-import {auth} from "../../components/header/header";
 
-// TODO: investigate further if we should use "use server" to avoid leaks of credentials
-function Login() {
-  // Get authentication instance
-  //const auth = getAuth();
-  // the authentication is obtained now from the header
+/* apparently you can put the api key in the client as long as you enforce what a user can read
+ * and write with https://console.firebase.google.com/u/0/project/test-quests-a3712/firestore/rules .
+ * See https://firebase.google.com/docs/projects/api-keys */
+export default function Login() {
+	const [error, setError] = useState(null); // State for managing error messages
 
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		setError(null)
 
+		const formData = new FormData(event.target)
+		const email = formData.get("email")
+		const password = formData.get("password")
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      const userCredential = await signInWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      const user = userCredential.user;
-      window.location.href = "/";
-    } catch (error) {
-      console.error("Error:", error.message);
-    }
-  };
+		if (!email || !password) {
+			setError("Please fill all the fields.")
+			return
+		}
 
-  return (
-    <div className="bg-cover bg-center flex justify-center items-center min-h-screen">
-      <div className="bg-black bg-opacity-80 text-white w-96 p-8 rounded-lg shadow-md mt-[-150px]">
-        <h1 className="mb-2 text-3xl text-center font-bold">Login</h1>
-        <form onSubmit={handleSubmit} className="flex flex-col space-y-4">
-          <label className="flex flex-col text-white">
-            <span className="mb-1">Email:</span>
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="border rounded px-2 py-1 text-black"
-            />
-          </label>
-          <label className="flex flex-col text-white">
-            <span className="mb-1">Password:</span>
-            <input
-              className="border rounded px-2 py-1 text-black"
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </label>
-          <button
-            type="submit"
-            className="bg-tq-primary hover:bg-tq-accent text-white font-bold py-2 px-4 rounded"
-          >
-            Submit
-          </button>
-          <div className="text-center mt-4">
-            Don't have an account?
-            <Link href={"#"} className="text-white-500 hover:underline">
-              Sign up here
-            </Link>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+		try {
+			await signInWithEmailAndPassword(getAuth(app), email, password)
+		} catch (error) {
+			switch (error.code) {
+				case 'auth/invalid-credential':
+					setError("Invalid e-mail or password.")
+					break;
+				default:
+					// TODO: maybe handle more errors like network errors
+					setError(error.message)
+					break;
+			}
+		}
+	};
+
+	return (
+		<div className="bg-cover bg-center flex justify-center items-center min-h-screen">
+			<div className="bg-black bg-opacity-80 text-white w-96 p-8 rounded-lg shadow-md mt-[-150px]">
+				<h1 className="mb-2 text-3xl text-center font-bold">Login</h1>
+				<form onSubmit={handleSubmit} className="flex flex-col space-y-4">
+					<label className="flex flex-col text-white">
+						<span className="mb-1">Email:</span>
+						<input
+							type="email"
+							name="email"
+							className="border rounded px-2 py-1 text-black"
+						/>
+					</label>
+					<label className="flex flex-col text-white">
+						<span className="mb-1">Password:</span>
+						<input
+							className="border rounded px-2 py-1 text-black"
+							name="password"
+							type="password"
+						/>
+					</label>
+					<button
+						type="submit"
+						className="bg-tq-primary hover:bg-tq-accent text-white font-bold py-2 px-4 rounded"
+					>
+						Submit
+					</button>
+					{error && <div className="text-red-500 mb-4">{error}</div>} {/* Display error message */}
+					<div className="text-center mt-4">
+						Don't have an account?
+						<Link href={"#"} className="text-white-500 hover:underline">
+							Sign up here
+						</Link>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
 }
-
-export default Login;
