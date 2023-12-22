ea29474bf581d679b171da31d87b22178f15d686
Giacomo <giacomo.ratto00@gmail.com>
2023-12-22 10:44:42 +0100

    better error handling in registration and use of db on server side

--- next-app/src/app/Registration/page.js
+++ next-app/src/app/Registration/page.js
@@ -1,102 +1,71 @@
 "use client";
 import React, { useState } from "react";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import { db } from "../../firebase/index";
-import { doc, setDoc, collection, query, where, getDocs } from "firebase/firestore";
-// import the auth from the header
-import { auth } from "../../components/header/header";
 
-//const auth = getAuth();
+import { registerUser } from "@/app/actions"
+
 
 function Registration() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [username, setUsername] = useState("");
-  const [error, setError] = useState(null); // State for managing error messages
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [username, setUsername] = useState("");
+	const [error, setError] = useState(null); // State for managing error messages
 
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+	const handleSubmit = async (event) => {
+		event.preventDefault();
 
-    try {
-      const userCredential = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      const user = userCredential.user;
-      // we need to store the username
-      const q = query(collection(db, "users"), where("username", "==", username));
+		const error = registerUser(email, password, username);
+		if (error)
+			setError(error)
+	};
 
-      const querySnapshot = await getDocs(q);
-
-      // find if the username is already present and if is not present in the snapshot returned by the query,
-      // then complite the registration of the user
-      if (querySnapshot.empty) {
-        // Add a new document in collection "users" with the selected username and an empty score
-        // Since email is unique, as ID of the document we set the user email
-        await setDoc(doc(db, "users", email), {
-          username: username,
-          score: 0,
-        });
-        window.location.href = "/";
-      }
-      else {
-        // print username already taken TOFO: imlement an alert
-        setError('Username already taken');
-      }
-    } catch (error) {
-      setError(error.message);
-    }
-};
-
-return (
-  <div className="bg-cover bg-center flex justify-center items-center min-h-screen">
-    <div className="bg-black bg-opacity-80 text-white w-96 p-8 rounded-lg shadow-md mt-[-150px]">
-      <h1 className="mb-2 text-3xl text-center font-bold">Registration</h1>
-      <form onSubmit={handleSubmit} className="flex flex-col space-y-4">
-        <label className="flex flex-col text white">
-          <span className="mb-1">Username:</span>
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            className="border rounded px-2 py-1 text-black"
-          //required
-          />
-        </label>
-        <label className="flex flex-col text white">
-          <span className="mb-1">Email:</span>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="border rounded px-2 py-1 text-black"
-          //required
-          />
-        </label>
-        <label className="flex flex-col text-white">
-          <span className="mb-1">Password:</span>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="border rounded px-2 py-1 text-black"
-            //required
-            minLength="6"
-          />
-        </label>
-        <button
-          type="submit"
-          className="bg-tq-primary hover:bg-tq-accent text-white font-bold py-2 px-4 rounded"
-        >
-          Submit
-        </button>
-        {error && <div className="text-red-500 mb-4">{error}</div>} {/* Display error message */}
-      </form>
-    </div>
-  </div>
-);
+	return (
+		<div className="bg-cover bg-center flex justify-center items-center min-h-screen">
+			<div className="bg-black bg-opacity-80 text-white w-96 p-8 rounded-lg shadow-md mt-[-150px]">
+				<h1 className="mb-2 text-3xl text-center font-bold">Registration</h1>
+				<form onSubmit={handleSubmit} className="flex flex-col space-y-4">
+					<label className="flex flex-col text white">
+						<span className="mb-1">Username:</span>
+						<input
+							type="text"
+							value={username}
+							onChange={(e) => setUsername(e.target.value)}
+							className="border rounded px-2 py-1 text-black"
+						//required
+						/>
+					</label>
+					<label className="flex flex-col text white">
+						<span className="mb-1">Email:</span>
+						<input
+							type="email"
+							value={email}
+							onChange={(e) => setEmail(e.target.value)}
+							className="border rounded px-2 py-1 text-black"
+						//required
+						/>
+					</label>
+					<label className="flex flex-col text-white">
+						<span className="mb-1">Password:</span>
+						<input
+							type="password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+							className="border rounded px-2 py-1 text-black"
+							//required
+							minLength="6"
+						/>
+					</label>
+					<button
+						type="submit"
+						className="bg-tq-primary hover:bg-tq-accent text-white font-bold py-2 px-4 rounded"
+					>
+						Submit
+					</button>
+					{error && <div className="text-red-500 mb-4">{error}</div>} {/* Display error message */}
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default Registration;
--- next-app/src/app/actions.js
+++ next-app/src/app/actions.js
@@ -1,7 +1,8 @@
 "use server"
 
-import { db } from "../firebase/index";
-import { doc, getDoc } from "firebase/firestore";
+import { db, app } from "../firebase/index";
+import { doc, getDoc, getDocs, setDoc, query, collection, where } from "firebase/firestore";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 
 export async function getExerciseData(exerciseName) {
@@ -9,4 +10,39 @@ export async function getExerciseData(exerciseName) {
 	const document = await getDoc(docRef);
 
 	return document.data()
+}
+
+export async function registerUser(email, password, username) {
+	const auth = getAuth(app);
+
+	if (!email || !password || !username)
+		return 'Please fill all the fields.'
+
+	if (password.length < 6)
+		return 'Password is too short. It must contain at least 6 characters.'
+
+	try {
+		const querySnapshot = await getDocs(query(collection(db, "users"), where("username", "==", username)));
+		if (querySnapshot.empty) {
+			await createUserWithEmailAndPassword(
+				auth,
+				email,
+				password
+			);
+			await setDoc(doc(db, "users", email), {
+				username: username,
+				score: 0,
+			});
+		} else {
+			console.error("how this happened??")
+		}
+	} catch (error) {
+		switch (error.code) {
+			case "auth/email-already-in-use":
+				return "An account is already associated with this email or this username";
+			default:
+				console.error(error)
+		}
+	}
+	return null
 }
