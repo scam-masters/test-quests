f9351cd95537f320099afb94488b1ef9589f8e56
LLaCorte <lorenzolacorte99@gmail.com>
2023-12-26 10:46:03 +0100

    Update tests on the landing page

--- test/tests/test_landing.py
+++ test/tests/test_landing.py
@@ -20,6 +20,11 @@ def expected_title():
 
 
 @pytest.fixture(scope="class")
+def user_50_points():
+    return ("test_50_points_user@gmail.com", "test_50_points_user")
+
+
+@pytest.fixture(scope="class")
 def driver():
     _driver = webdriver.Chrome(ChromeDriverManager().install())
     yield _driver
@@ -32,23 +37,23 @@ def load_page(base_url, expected_title, driver):
     WebDriverWait(driver, 10).until(EC.title_contains(expected_title))
 
 
-@pytest.fixture(scope="class", autouse=True)
-def login(driver):
-    driver.find_element(By.XPATH, "//*[@id=\"email\"]").send_keys('tests@gmail.com')
-    driver.find_element(By.XPATH, "//*[@id=\"password\"]").send_keys('testtest')
-    driver.find_element(By.XPATH, "/html/body/div[1]/div/form/button").click()
+def wait_landing_render(driver):
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div[1]/a/button")))
 
 
-class TestLandingPageSuccess:
-    def test_working_page(self, driver, expected_title):
-        assert expected_title in driver.title
+@pytest.fixture(scope="class", autouse=True)
+def login(driver, user_50_points):
+    driver.find_element(By.XPATH, "//*[@id=\"email\"]").send_keys(user_50_points[0])
+    driver.find_element(By.XPATH, "//*[@id=\"password\"]").send_keys(user_50_points[1])
+    driver.find_element(By.XPATH, "/html/body/div[1]/div/form/button").click()
+    wait_landing_render(driver)
 
-    def test_first_mission(self, driver):
+
+class TestLandingPageSuccess:
+    def test_first_mission_unlocked(self, driver):
         first_circle_mission = driver.find_element(
             By.XPATH, "/html/body/div[1]/div[1]/a/button"
         )
-        assert "Path Traversal" in first_circle_mission.text
 
         """ Check for accessing to the first learning page """
         first_circle_mission.click()
@@ -59,12 +64,32 @@ class TestLandingPageSuccess:
         )
 
         assert driver.find_element(By.XPATH, go_back_button_xpath).text == "Go back to main page"
+        driver.find_element(By.XPATH, go_back_button_xpath).click()
+        wait_landing_render(driver)
+
+    def test_mission_points(self, driver):
+        first_circle_mission = driver.find_element(
+            By.XPATH, "/html/body/div[1]/div[1]/a/button"
+        )
+
+        second_circle_mission = driver.find_element(
+            By.XPATH, "/html/body/div[1]/div[2]/a/button"
+        )
+
+        assert "Path Traversal" in first_circle_mission.text and "Login Bypass" in second_circle_mission.text
+        assert "50/50" in first_circle_mission.text and "0/50" in second_circle_mission.text
+
+    def test_header_points(self, driver):
+        header_points = driver.find_element(
+            By.XPATH, "/html/body/header/div[1]/div/span[2]"
+        )
+        assert "50" in header_points.text
 
 
 class TestLandingPageUnsuccess:
     def test_mission_locked(self, driver):
         locked_circle_mission = driver.find_element(
-            By.XPATH, "/html/body/div[1]/div[2]/button"
+            By.XPATH, "/html/body/div[1]/div[3]/button"
         )
         assert locked_circle_mission.text == "Mission Locked"
 
