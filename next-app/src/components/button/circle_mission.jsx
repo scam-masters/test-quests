f33338a3a202b15c316196b7dc05089e95cdc928
Giacomo <giacomo.ratto00@gmail.com>
2023-12-26 19:26:46 +0100

    fix score on landing page being -1

--- next-app/src/components/button/circle_mission.jsx
+++ next-app/src/components/button/circle_mission.jsx
@@ -1,9 +1,11 @@
 "use client";
 import React from 'react';
 
-function CircleMission({ children, type, userScore, maxPoints }){
-	let currentScore = String(userScore).concat(String('/')).concat(String(maxPoints));
-	currentScore = maxPoints ? currentScore : null;
+function CircleMission({ children, type, userScore, maxPoints }) {
+	let currentScore = null
+	if (userScore >= 0 && maxPoints) {
+		currentScore = String(userScore) + '/' + maxPoints;
+	}
 	return (
 		<button className={'circle-mission-' + type}>
 			{children}
