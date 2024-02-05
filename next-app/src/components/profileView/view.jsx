cec9d9663cb93160927e08fb90de906a8f9193a5
LoScarro <davisca2000@gmail.com>
2024-02-05 15:11:12 +0100

    profile view style enhanced

--- next-app/src/components/profileView/view.jsx
+++ next-app/src/components/profileView/view.jsx
@@ -6,43 +6,45 @@ import { getAuth } from "firebase/auth";
 // import { updateAvatar, updateUsername } from "@/app/user_actions.js"
 // import { Dialog } from "primereact/dialog";
 
-export default function ProfileView({ email, avatar, badges, username, score}) {
+export default function ProfileView({ email, avatar, badges, username, score }) {
 
-	const [isOwner, setIsOwner] = useState(false);
+    const [isOwner, setIsOwner] = useState(false);
 
     useEffect(() => {
         getAuth().onAuthStateChanged(function (user) {
-			if (user) {
-				setIsOwner(user.email === email);
-			} else {
-				setIsOwner(false);
-			}
-		});
-	}, []);
+            if (user) {
+                setIsOwner(user.email === email);
+            } else {
+                setIsOwner(false);
+            }
+        });
+    }, []);
 
     return (
-        <>
-            <div className="flex flex-row items-center">
-                <img src={`/avatars/${avatar}.png`} alt={`avatar${avatar}`} className="w-20 h-20 rounded-full m-4" />
-                {isOwner ? <Button text="Change avatar"/> : ""}
+        <div className="flex flex-row tems-center justify-center m-auto p-4">
+            <div className="flex flex-col items-center justify-center">
+                <img src={`/avatars/${avatar}.png`} alt={`avatar${avatar}`} className="rounded-full p-6" />
 
-                <div className="ml-4">
-                    <h1 className="text-3xl font-bold text-blue-700">{username}</h1>
-                    {isOwner ? <Button text="Change username"/> : ""}
+                {isOwner && <Button type='blue'>Change Avatar</Button>}
+            </div>
 
-                    {isOwner ? <p className="text-xl text-gray-600">{email}</p>: ""}
+            <div className="ml-5 mt-auto mb-auto">
+                <div className="flex flex-row items-center">
 
-                    <p className="text-xl text-gray-600">{score}</p>
+                    <h1 className="text-5xl font-bold mr-8">{username}</h1>
+                    {isOwner && <Button type='blue'>Change Username</Button>}
+                
+                </div>
+                {isOwner && <p className="text-xl mt-4">Email: {email}</p>}
 
-                    <div className="flex flex-row mt-4">
-                        {badges.map((badge, index) => {
-                            return (
-                                <img key={index} src={`/badges/${badge}.png`} alt={`badge${badge}`} className="w-10 h-10 mr-2" />
-                            );
-                        })}
-                    </div>
+                <p className="text-xl mt-4">Score: {score}</p>
+
+                <div className="flex flex-row ">
+                    {badges.map((badge, index) => (
+                        <img key={index} src={`/badges/${badge}.png`} alt={`badge${badge}`} className="w-10 h-10 mr-2 mt-4" />
+                    ))}
                 </div>
             </div>
-        </>
+        </div>
     );
 }
