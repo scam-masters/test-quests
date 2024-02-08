9a66d4ae48389137923ee0533f779f37e94fefb5
Abdul Jalil <127906336+jalilrohi1@users.noreply.github.com>
2024-02-08 16:04:55 +0100

    Fixing Design Bug Making the Learning Page Responsive and Center it

--- next-app/src/app/exercise/[mission]/page.js
+++ next-app/src/app/exercise/[mission]/page.js
@@ -1,92 +1,96 @@
-"use server"
-import React from 'react';
+"use server";
+import React from "react";
 import "primereact/resources/themes/bootstrap4-dark-blue/theme.css";
 
-import ExerciseView from '@/components/exerciseView/view'
-import DropdownExercise from '@/components/DropdownExercise/exercise'
-import DragAndDropExercise from '@/components/DragAndDropExercise/page'
-import DragAndDropMmExercise from '@/components/DragAndDropMultipleMatching/page'
-import OpenCloseExercise from '@/components/OpenCloseExercise/page'
-import DebugExercise from '@/components/DebugExercise/page';
+import ExerciseView from "@/components/exerciseView/view";
+import DropdownExercise from "@/components/DropdownExercise/exercise";
+import DragAndDropExercise from "@/components/DragAndDropExercise/page";
+import DragAndDropMmExercise from "@/components/DragAndDropMultipleMatching/page";
+import OpenCloseExercise from "@/components/OpenCloseExercise/page";
+import DebugExercise from "@/components/DebugExercise/page";
 
-import { getExerciseData } from "@/app/actions"
+import { getExerciseData } from "@/app/actions";
 
 // https://nextjs.org/docs/app/building-your-application/routing/dynamic-routes
 export default async function Exercise({ params }) {
-    // Get the current mission
-    const missionContent = await getExerciseData(`mission_${params.mission}`);
+  // Get the current mission
+  const missionContent = await getExerciseData(`mission_${params.mission}`);
 
-    let exercise;
-    let exerciseArgs;
+  let exercise;
+  let exerciseArgs;
 
-    switch (missionContent.type) {
-        case 'dnd':
-            exercise = DragAndDropExercise
-            exerciseArgs = {
-                explanation: missionContent.explanation,
-                blocks: missionContent.blocks,
-                solution: missionContent.solution,
-                options: (missionContent.options || missionContent.solution).toSorted(() => Math.random() - 0.5), // shuffle to obtain options
-            }
-            break;
-        case 'sd':
-            exercise = DropdownExercise
-            exerciseArgs = {
-                dropdowns: missionContent.dropdowns,
-                explanation: missionContent.explanation,
-                name: missionContent.name,
-                text: missionContent.text,
-            }
-            break;
-        case 'mm':
-            exercise = DragAndDropMmExercise
-            exerciseArgs = {
-                options: missionContent.solution.toSorted(() => Math.random() - 0.5), // shuffle to obtain options
-                solution: missionContent.solution,
-                blocks: missionContent.blocks,
-            }
-            break;
-        case 'oc':
-            exercise = OpenCloseExercise
-            exerciseArgs = {
-                blocks: missionContent.blocks,
-                solution: missionContent.solution
-            }
-            break;
-        case 'debug':
-            exercise = DebugExercise
-            exerciseArgs = {
-                text: missionContent.text,
-                selectables: missionContent.selectables.map(selectable => {
-                    return selectable.split("-").map(s => parseInt(s))
-                }),
-                solution: missionContent.solution
-            }
-    }
+  switch (missionContent.type) {
+    case "dnd":
+      exercise = DragAndDropExercise;
+      exerciseArgs = {
+        explanation: missionContent.explanation,
+        blocks: missionContent.blocks,
+        solution: missionContent.solution,
+        options: (missionContent.options || missionContent.solution).toSorted(
+          () => Math.random() - 0.5
+        ), // shuffle to obtain options
+      };
+      break;
+    case "sd":
+      exercise = DropdownExercise;
+      exerciseArgs = {
+        dropdowns: missionContent.dropdowns,
+        explanation: missionContent.explanation,
+        name: missionContent.name,
+        text: missionContent.text,
+      };
+      break;
+    case "mm":
+      exercise = DragAndDropMmExercise;
+      exerciseArgs = {
+        options: missionContent.solution.toSorted(() => Math.random() - 0.5), // shuffle to obtain options
+        solution: missionContent.solution,
+        blocks: missionContent.blocks,
+      };
+      break;
+    case "oc":
+      exercise = OpenCloseExercise;
+      exerciseArgs = {
+        blocks: missionContent.blocks,
+        solution: missionContent.solution,
+      };
+      break;
+    case "debug":
+      exercise = DebugExercise;
+      exerciseArgs = {
+        text: missionContent.text,
+        selectables: missionContent.selectables.map((selectable) => {
+          return selectable.split("-").map((s) => parseInt(s));
+        }),
+        solution: missionContent.solution,
+      };
+  }
 
-    /* Placeholder content for exercise explanation */
-    const exerciseExplanation = (
-        <div className="p-4 text-white" dangerouslySetInnerHTML={{ __html: missionContent.explanation }}>
-        </div>
-    );
+  /* Placeholder content for exercise explanation */
+  const exerciseExplanation = (
+    <div
+      className="p-4 text-white"
+      dangerouslySetInnerHTML={{ __html: missionContent.explanation }}
+    ></div>
+  );
 
-    /* Placeholder content for exercise link to its chapter page */
-    const missionChapter = "/chapter/" + missionContent.difficulty;
+  /* Placeholder content for exercise link to its chapter page */
+  const missionChapter = "/chapter/" + missionContent.difficulty;
 
-    return (
-        <div>
-            {/* Use the LearningComponent with mission-specific content */}
-            <ExerciseView
-                missionId={missionContent.id}
-                missionChapter={missionChapter}
-                exerciseExplanation={exerciseExplanation}
-                resource={missionContent.resourceLink}
-                Exercise={exercise}
-                exerciseArguments={exerciseArgs}
-                exercisePoints={missionContent.points}
-                exerciseThreshold={missionContent.threshold}
-                time={missionContent.time}
-            />
-        </div>
-    );
+  return (
+    <div>
+      {/* Use the LearningComponent with mission-specific content */}
+      <ExerciseView
+        missionId={missionContent.id}
+        missionChapter={missionChapter}
+        exerciseExplanation={exerciseExplanation}
+        resource={missionContent.resourceLink}
+        Exercise={exercise}
+        exerciseArguments={exerciseArgs}
+        exercisePoints={missionContent.points}
+        exerciseThreshold={missionContent.threshold}
+        time={missionContent.time}
+      />
+    </div>
+  );
 }
--- next-app/src/components/Learning/learning.jsx
+++ next-app/src/components/Learning/learning.jsx
@@ -9,7 +9,7 @@ export default function LearningComponent({
 }) {
 	return (
 		<div className="text-white text-left">
-			<div className="flex justify-between w-4/5 mx-auto mb-8 mt-8">
+			<div className="flex justify-center w-4/5 mx-auto mb-8 mt-8">
 				{contentHTML}
 			</div>
 			<div className="grid grid-cols-3 justify-between w-4/5 mx-auto mb-8">
