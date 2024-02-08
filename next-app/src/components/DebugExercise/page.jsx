6c37e7cc058511c7d41a21a0121b8e5ae96d7e49
Giacomo <giacomo.ratto00@gmail.com>
2024-02-08 10:19:31 +0100

    fix debug exercise

--- next-app/src/components/DebugExercise/page.jsx
+++ next-app/src/components/DebugExercise/page.jsx
@@ -3,56 +3,50 @@
 import { useState } from "react"
 
 export default function DebugExercise({ text, selectables, solution, onScoreComputed }) {
-    const [answers, setAnswers] = useState(Array(selectables.length).fill(false))
+	const [answers, setAnswers] = useState(Array(selectables.length).fill(false))
 
-    function click(i) {
-        setAnswers(prev => {
-            let arr = Array.from(prev);
-            arr[i] = !arr[i];
-            return arr;
-        })
-    }
+	function click(i) {
+		setAnswers(prev => {
+			let arr = Array.from(prev);
+			arr[i] = !arr[i];
+			return arr;
+		})
+	}
 
-    function handleSubmit(e) {
-        e.preventDefault()
-        let count = 0
-        for (let i = 0; i < answers.length; i++) {
-            if (answers[i] == solution[i]) {
-                count++;
-            }
-        }
-        onScoreComputed(100 * count / answers.length);
-    }
+	function handleSubmit(e) {
+		e.preventDefault()
+		let count = 0
+		for (let i = 0; i < answers.length; i++) {
+			if (answers[i] == solution[i]) {
+				count++;
+			}
+		}
+		onScoreComputed(100 * count / answers.length);
+	}
 
-    let i = 0;
-    let exercise = [];
-    for (const [index, item] of selectables.entries()) {
-        let unselectable = text.substring(i, item[0] - 1);
-        let selectable =`
-        <button key="selectable_${index}"
-                class="${answers[index] ? 'bug-selected' : 'bug-selectable'}"
-                onClick="click(${index})">
-          ${text.substring(item[0], item[1])}
-        </button>
-      `;
+	let i = 0;
+	let exercise = [];
+	for (const [index, item] of selectables.entries()) {
+		console.log(index, item)
+		let unselectable = text.substring(i, item[0]);
+		let selectable = <button key="selectable_${index}"
+			class={answers[index] ? 'bug-selected' : 'bug-selectable'}
+			onClick={click.bind(null, index)}>
+			{text.substring(item[0], item[1])}
+		</button>
 
-        i = item[1];
-        exercise.push(unselectable);
-        exercise.push(selectable);
-        
-        console.log("unselectable: ",unselectable);
-        console.log("selectable: ", selectable);
 
-    }
-    // exercise.push(text.substring(i).replace(/"\n"/g, "<br>"));    now the <br> are directly in the text
-    exercise.push(text);
-    
-    
-    return (
-        <>
-            <form id='exercise-form' onSubmit={handleSubmit}>
-            </form>
-            <div dangerouslySetInnerHTML={{ __html: exercise}} />;
-        </>
-    )
+		i = item[1];
+		exercise.push(unselectable);
+		exercise.push(selectable);
+	}
+	exercise.push(text.substring(i));
+
+
+	return (
+		<>
+			<form id='exercise-form' onSubmit={handleSubmit}></form>
+			<code>{exercise}</code>
+		</>
+	)
 }
