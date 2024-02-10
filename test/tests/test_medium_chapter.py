4ac64f06668861d68a136596a01c8286fb221b54
Giacomo Pedemonte <giacomopedemonte@libero.it>
2024-02-10 20:37:26 +0100

    medium chapter tests and timer

---
+++ test/tests/test_medium_chapter.py
@@ -0,0 +1,68 @@
+import pytest
+from selenium.webdriver.common.by import By
+
+
+from utils import login, wait_landing_render
+
+
+@pytest.fixture(scope="class", autouse=True)
+def login_user_50_points(driver, user_all_unlocked2):
+    login(driver, user_all_unlocked2[0], user_all_unlocked2[1])
+
+
+# before each method we need to navigate to the correct page!!!!
+@pytest.fixture(scope="function", autouse=True)
+def navigate_to_easy_chapter(driver, base_url):
+    driver.get(base_url + "/chapter/medium")
+    driver.find_element(By.XPATH, "//button[contains(text(),'SQL Injection UNION Attacks')]")
+
+
+class TestChapterPageSuccess:
+    def test_first_mission_unlocked(self, driver):
+        first_circle_mission = driver.find_element(
+            By.XPATH, "//button[@class='circle-mission-gradient-1 -translate-x-[110%]']"
+        )
+
+        """ Check for accessing to the first learning page """
+        first_circle_mission.click()
+
+        go_back_button_xpath = driver.find_element(
+            By.XPATH, "/html/body/div[1]/div/div[2]/a[1]/button"
+        )
+
+        assert go_back_button_xpath.text == "Go back to chapter page"
+        go_back_button_xpath.click()
+        wait_landing_render(driver)
+
+    def test_mission_points(self, driver):
+        first_circle_mission = driver.find_element(
+            By.XPATH, "/html/body/div[1]/div[1]/a/button"
+        )
+
+        second_circle_mission = driver.find_element(
+            By.XPATH, "//button[@class='circle-mission-locked -translate-x-[150%]']" # the mission locked is not working
+            # "/html/body/div[1]/div[2]/a/button" not recognizable in firefox
+        )
+
+        assert (
+            "SQL Injection UNION Attacks" in first_circle_mission.text
+            and "Mission Locked" in second_circle_mission.text
+        )
+        assert (
+            "50/50" in first_circle_mission.text
+            and "0/50" in second_circle_mission.text
+        )
+
+class TestChapterPageUnsuccess:
+    def test_mission_locked(self, driver):
+        locked_circle_mission = driver.find_element(
+            By.XPATH, "//button[@class='circle-mission-locked -translate-x-[150%]']"
+        )
+        assert locked_circle_mission.text == "Mission Locked"
+
+        """ Check for accessing to the first learning page """
+        locked_circle_mission.click()
+        popup_element = driver.find_element(
+            By.XPATH, "/html/body/div[3]/div/div[1]/div[1]"
+        )
+        assert popup_element.text == "Mission Locked"
---
+++ test/tests/test_timer.py
@@ -0,0 +1,38 @@
+import pytest
+from selenium.webdriver.common.by import By
+
+
+from utils import login, wait_landing_render
+import time
+from utils import get_exercise_popup
+
+
+@pytest.fixture(scope="class", autouse=True)
+def login_user_50_points(driver, user_all_unlocked):
+    login(driver, user_all_unlocked[0], user_all_unlocked[1])
+
+
+# before each method we need to navigate to the correct page!!!!
+@pytest.fixture(scope="function", autouse=True)
+def navigate_to_easy_chapter(driver, base_url):
+    driver.get(base_url + "/exercise/7")
+    timer = driver.find_element(
+            By.ID, "timer"
+        )
+    return timer
+    
+class Timer:
+    def test_timer_visualization(self, navigate_to_easy_chapter):  # pass the timer element as an argument
+        timer = navigate_to_easy_chapter  # assign the timer element to a variable
+        assert timer.is_displayed(), "Timer is not displayed"
+        
+    def test_timer_countdown(self, navigate_to_easy_chapter, driver):
+        timer = navigate_to_easy_chapter
+        #assert timer.text == "00:00", "Timer is not counting down"
+        
+        # wait for 1 minute to make the timer expire and check if the popup arises
+        time.sleep(61)
+        
+        popup = get_exercise_popup(driver)
+
+        assert "CHEATER" in popup.text
