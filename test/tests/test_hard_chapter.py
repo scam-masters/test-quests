e7a090f7116c3db44f0a82265612285f71d600f2
Abdul Jalil <127906336+jalilrohi1@users.noreply.github.com>
2024-02-13 22:37:15 +0100

    test Chapter 3 rifining

--- test/tests/test_hard_chapter.py
+++ test/tests/test_hard_chapter.py
@@ -3,20 +3,20 @@ from selenium.webdriver.common.by import By
 
 
 from utils import login
-from utils import wait_chapter2_landing_render
+from utils import wait_chapter3_landing_render
 
 
 @pytest.fixture(scope="class", autouse=True)
-def login_user_all_unlocked2(driver, user_all_unlocked2):
-    login(driver, user_all_unlocked2[0], user_all_unlocked2[1])
+def login_user_all_unlocked(driver, user_all_unlocked):
+
+    login(driver, user_all_unlocked[0], user_all_unlocked[1])
 
 
 # before each method we need to navigate to the correct page!!!!
 @pytest.fixture(scope="function", autouse=True)
 def navigate_to_hard_chapter(driver, base_url):
     driver.get(base_url + "/chapter/hard")
-    print("Current url: ", driver.current_url)
-    
+
 
 class TestChapterPageSuccess:
     def test_first_mission_unlocked(self, driver):
@@ -25,12 +25,11 @@ class TestChapterPageSuccess:
         )
 
         """ Check for accessing to the first learning page """
-        first_circle_mission.click()
-        driver.implicitly_wait()
-        go_back_button_xpath = driver.find_element(
-            By.ID, 'back_to_main'
-        )
-        print(go_back_button_xpath.text)
-        assert go_back_button_xpath.text == "Go back to chapter page"
+
+        go_back_button_xpath = driver.find_element(By.ID, "back_to_main")
+
+        # print(go_back_button_xpath.text)
+        assert go_back_button_xpath.text == "Go back to main page"
+
         go_back_button_xpath.click()
-        wait_chapter2_landing_render(driver)
+        wait_chapter3_landing_render(driver)
