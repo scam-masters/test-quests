c0e3eb1a41b888ec795b1f000a7afcf9fb905a7a
Giorgio Rengucci <37120983+GiorgioRen@users.noreply.github.com>
2024-02-13 16:40:30 +0100

    add friends design

--- next-app/src/components/profileView/view.jsx
+++ next-app/src/components/profileView/view.jsx
@@ -25,55 +25,84 @@ export default function ProfileView({ email, avatar, badges, username, friends, 
                 setErrorMessage(error.message);
             });
     };
-    
+
     return (
-        <div className="flex flex-row tems-center justify-center m-auto p-4">
-            <div className="flex flex-col items-center justify-center">
-                <img src={`/avatars/${avatar}.png`} alt={`avatar${avatar}`} className="rounded-full m-3 w-40 h-40 border-2 border-white" />
+        <div className="flex flex-col items-center justify-center m-auto p-4 w-full ">
+            <div className="flex flex-row tems-center justify-center p-4">
+                <div className="flex flex-col items-center justify-center">
+                    <img src={`/avatars/${avatar}.png`} alt={`avatar${avatar}`} className="rounded-full m-3 w-40 h-40 border-2 border-white" />
 
-                {isOwner && <Button type='blue' href="/changeAvatar">Change Avatar</Button>}
+                    {isOwner && <Button type='blue' href="/changeAvatar">Change Avatar</Button>}
+                </div>
+
+                <div className="ml-5 mt-auto mb-auto">
+                    <div className="flex flex-row items-center">
+
+                        <h1 className="text-5xl font-bold mr-8">{username}</h1>
+                        {isOwner && <Button type='blue' href="/changeUsername">Change Username</Button>}
+
+                    </div>
+                    {isOwner && <p className="text-xl mt-4">Email: {email}</p>}
+
+                    <p className="text-xl mt-4">Score: {score}</p>
+
+                    <div className="flex flex-row ">
+                        {badges.map((badge, index) => (
+                            <img title={badge.replace("_", " ")} key={index} src={`/badges/${badge}.png`} alt={`badge${badge}`} className="w-10 h-10 mr-2 mt-4" />
+                        ))}
+                    </div>
+                </div>
             </div>
 
-            <div className="ml-5 mt-auto mb-auto">
-                <div className="flex flex-row items-center">
+            {
+                isOwner ? null : (
+                    <div className="flex justify-center p-4 flex-col">
+                        <Button type='blue' onClick={handleAddFriend} id="add_friend">Add Friend</Button>
+                        {errorMessage && <div className="error-message">{errorMessage}</div>}
+                    </div>
+                )
+            }
 
-                    <h1 className="text-5xl font-bold mr-8">{username}</h1>
-                    {isOwner && <Button type='blue' href="/changeUsername">Change Username</Button>}
-                
+            <div className="flex w-8/12 mx-auto border-2 border-white rounded-xl">
+                <div className="w-1/2 max-w-1/2 p-3 min-h-[30vh] flex justify-center">
+                    <div className="">
+                        <h2 className="text-3xl font-bold">Friends</h2>
+                        {friends.length > 0 ? (
+                            <ul>
+                                {friends.map((friend, index) => (
+                                    <>
+                                        <a href={`/profile/${friend}`}>
+                                            <li key={index} className="flex items-center mb-2 ">
+                                                <img src={`/avatars/0.png`} alt={`avatar`} className="rounded-full w-8 h-8 mr-2" />
+                                                <span className="text-lg">{friend}</span>
+                                            </li>
+                                        </a>
+                                    </>
+                                ))}
+                            </ul>
+                        ) : (
+                            <p>No friends found.</p>
+                        )}
+                    </div>
                 </div>
-                {isOwner && <p className="text-xl mt-4">Email: {email}</p>}
-
-                <p className="text-xl mt-4">Score: {score}</p>
-
-                <div className="flex flex-row ">
-                    {badges.map((badge, index) => (
-                        <img title={badge.replace("_", " ")} key={index} src={`/badges/${badge}.png`} alt={`badge${badge}`} className="w-10 h-10 mr-2 mt-4" />
-                    ))}
-                </div>
-            </div>
-
-            <div className="ml-5 mt-auto mb-auto">
-                <h2 className="text-3xl font-bold">Friends</h2>
-                <ul>
-                    {friends.map((friend, index) => (
-                        <li key={index}>{friend}</li>
-                    ))}
-                </ul>
-                {isOwner ? 
-                    (<>  
-                        <h2 className="text-3xl font-bold">Friend Requests</h2>
-                        <ul>
-                            {friendRequests.map((friend, index) => (
-                                <li key={index}>{friend}</li>
-                            ))}
-                        </ul>
-                    </>):
-                    (
-                        <>
-                            <Button type='blue' onClick={handleAddFriend} id="add_friend">Add Friend</Button>
-                            {errorMessage && <div className="error-message">{errorMessage}</div>}
-                        </>
-                    )
+                {isOwner ?
+                    (<>
+                        <div className="w-1/2 max-w-1/2 p-3 min-h-[30vh] border-l-2 border-white flex justify-center">
+                            <div className="">
+                                <h2 className="text-3xl font-bold">Friend Requests</h2>
+                                {friendRequests.length > 0 ? (
+                                    <ul>
+                                        {friendRequests.map((friend, index) => (
+                                            <li key={index}>{friend}</li>
+                                        ))}
+                                    </ul>
+                                ) : (
+                                    <p>No friend requests found.</p>
+                                )}
+                            </div>
+                        </div>
+                    </>) :
+                    (null)
                 }
             </div>
         </div>
