b3b5102a0e2eee8c87f96432173973cb362732a6
LLaCorte <lorenzolacorte99@gmail.com>
2024-02-19 15:33:01 +0100

    Nest storylines and chapters in the structure

--- next-app/src/app/actions.js
+++ next-app/src/app/actions.js
@@ -60,12 +60,16 @@ export async function getMissionList(language) {
 }
 
 // Retrieve the list of missions for a certain chapter (so for the difficulty level)
-export async function getMissionsByDifficulty(difficultyLevel) {
+export async function getChapterMissions(difficultyLevel, storyline) {
 	let missionList = [];
 	try {
 		// Create a query to filter documents based on difficulty
-		const q = query(collection(db, 'exercises'), where('difficulty', '==', difficultyLevel));
-
+		const q = query(
+			collection(db, 'exercises'), 
+			where('difficulty', '==', difficultyLevel),
+			where('storyline', '==', storyline)
+		);
+		
 		// Execute the query
 		const querySnapshot = await getDocs(q);
 
