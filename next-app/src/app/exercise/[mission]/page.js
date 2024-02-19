04132be2b5312f170f6db4315664152987f14f0b
Giacomo <giacomo.ratto00@gmail.com>
2024-02-19 14:52:38 +0100

    make exercise page client side

--- next-app/src/app/exercise/[mission]/page.js
+++ next-app/src/app/exercise/[mission]/page.js
@@ -1,5 +1,5 @@
-"use server";
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import "primereact/resources/themes/bootstrap4-dark-blue/theme.css";
 
 import ExerciseView from "@/components/exerciseView/view";
@@ -11,97 +11,104 @@ import DebugExercise from "@/components/DebugExercise/page";
 import MultipleSelection from "@/components/MultipleSelection";
 
 import { getExerciseData } from "@/app/actions";
+import Loading from "@/components/Loading";
 
 // https://nextjs.org/docs/app/building-your-application/routing/dynamic-routes
-export default async function Exercise({ params }) {
-  // Get the current mission
-  const missionContent = await getExerciseData(`mission_${params.mission}`);
+export default function Exercise({ params }) {
+	// Get the current mission
+	const [missionContent, setMissionContent] = useState(null)
+	useEffect(() => {
+		getExerciseData(`mission_${params.mission}`).then(setMissionContent)
+	})
 
-  let exercise;
-  let exerciseArgs;
-  let hint;
+	if (!missionContent)
+		return <Loading />
 
-  switch (missionContent.type) {
-    case "dnd":
-      exercise = DragAndDropExercise;
-      exerciseArgs = {
-        explanation: missionContent.explanation,
-        blocks: missionContent.blocks,
-        solution: missionContent.solution,
-        options: (missionContent.options || missionContent.solution).toSorted(
-          () => Math.random() - 0.5
-        ), // shuffle to obtain options
-      };
-      hint = "Drag and drop the blue blocks to fill all the blanks."
-      break;
-    case "sd":
-      exercise = DropdownExercise;
-      exerciseArgs = {
-        dropdowns: missionContent.dropdowns,
-        explanation: missionContent.explanation,
-        name: missionContent.name,
-        text: missionContent.text,
-      };
-      hint = "Select the correct option from the dropdowns to fill all the blanks."
-      break;
-    case "mm":
-      exercise = DragAndDropMmExercise;
-      exerciseArgs = {
-        options: missionContent.solution.toSorted(() => Math.random() - 0.5), // shuffle to obtain options
-        solution: missionContent.solution,
-        blocks: missionContent.blocks,
-      };
-      hint = "Reorder the blue blocks to match the statements on the left."
-      break;
-    case "oc":
-      exercise = OpenCloseExercise;
-      exerciseArgs = {
-        blocks: missionContent.blocks,
-        solution: missionContent.solution,
-      };
-      hint = "Write your answers in the empty spaces."
-      break;
-    case "debug":
-      exercise = DebugExercise;
-      exerciseArgs = {
-        text: missionContent.text,
-        selectables: missionContent.selectables.map((selectable) => {
-          return selectable.split("-").map((s) => parseInt(s));
-        }),
-        solution: missionContent.solution,
-      };
-      hint = "Select the buggy statements to fix the code."
-      break
-    case "ms":
-      exercise = MultipleSelection;
-      exerciseArgs = missionContent;
-      hint = "Select all the correct answers among the options."
-  }
+	let exercise;
+	let exerciseArgs;
+	let hint;
 
-  /* Placeholder content for exercise explanation */
-  const exerciseExplanation = (
-    <div
-      className="p-4 text-white"
-      dangerouslySetInnerHTML={{ __html: missionContent.explanation }}
-    ></div>
-  );
+	switch (missionContent.type) {
+		case "dnd":
+			exercise = DragAndDropExercise;
+			exerciseArgs = {
+				explanation: missionContent.explanation,
+				blocks: missionContent.blocks,
+				solution: missionContent.solution,
+				options: (missionContent.options || missionContent.solution).toSorted(
+					() => Math.random() - 0.5
+				), // shuffle to obtain options
+			};
+			hint = "Drag and drop the blue blocks to fill all the blanks."
+			break;
+		case "sd":
+			exercise = DropdownExercise;
+			exerciseArgs = {
+				dropdowns: missionContent.dropdowns,
+				explanation: missionContent.explanation,
+				name: missionContent.name,
+				text: missionContent.text,
+			};
+			hint = "Select the correct option from the dropdowns to fill all the blanks."
+			break;
+		case "mm":
+			exercise = DragAndDropMmExercise;
+			exerciseArgs = {
+				options: missionContent.solution.toSorted(() => Math.random() - 0.5), // shuffle to obtain options
+				solution: missionContent.solution,
+				blocks: missionContent.blocks,
+			};
+			hint = "Reorder the blue blocks to match the statements on the left."
+			break;
+		case "oc":
+			exercise = OpenCloseExercise;
+			exerciseArgs = {
+				blocks: missionContent.blocks,
+				solution: missionContent.solution,
+			};
+			hint = "Write your answers in the empty spaces."
+			break;
+		case "debug":
+			exercise = DebugExercise;
+			exerciseArgs = {
+				text: missionContent.text,
+				selectables: missionContent.selectables.map((selectable) => {
+					return selectable.split("-").map((s) => parseInt(s));
+				}),
+				solution: missionContent.solution,
+			};
+			hint = "Select the buggy statements to fix the code."
+			break
+		case "ms":
+			exercise = MultipleSelection;
+			exerciseArgs = missionContent;
+			hint = "Select all the correct answers among the options."
+	}
 
-  /* Placeholder content for exercise link to its chapter page */
-  const missionChapter = "/chapter/" + missionContent.difficulty;
+	/* Placeholder content for exercise explanation */
+	const exerciseExplanation = (
+		<div
+			className="p-4 text-white"
+			dangerouslySetInnerHTML={{ __html: missionContent.explanation }}
+		></div>
+	);
 
-  /* Use the LearningComponent with mission-specific content */
-  return (
-      <ExerciseView
-        missionId={missionContent.id}
-        missionChapter={missionChapter}
-        exerciseExplanation={exerciseExplanation}
-        resource={missionContent.resourceLink}
-        Exercise={exercise}
-        exerciseArguments={exerciseArgs}
-        exercisePoints={missionContent.points}
-        exerciseThreshold={missionContent.threshold}
-        time={missionContent.time}
-        hint={hint}
-      />
-  );
+	/* Placeholder content for exercise link to its chapter page */
+	const missionChapter = "/chapter/" + missionContent.difficulty;
+
+	/* Use the LearningComponent with mission-specific content */
+	return (
+		<ExerciseView
+			missionId={missionContent.id}
+			missionChapter={missionChapter}
+			exerciseExplanation={exerciseExplanation}
+			resource={missionContent.resourceLink}
+			Exercise={exercise}
+			exerciseArguments={exerciseArgs}
+			exercisePoints={missionContent.points}
+			exerciseThreshold={missionContent.threshold}
+			time={missionContent.time}
+			hint={hint}
+		/>
+	);
 }
