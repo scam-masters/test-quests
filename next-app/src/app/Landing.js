9b5288b9cd4e86463a17486dcf5e15ce13588e54
Giacomo <giacomo.ratto00@gmail.com>
2024-02-20 11:38:34 +0100

    add loading and fix broken link in learning page

--- next-app/src/app/Landing.js
+++ next-app/src/app/Landing.js
@@ -6,36 +6,40 @@ import { getAuth } from "firebase/auth";
 
 import { getStorylineList } from '@/app/actions';
 import CircleMission from '@/components/button/circle_mission';
+import Loading from '@/components/Loading';
 
 export default function Landing() {
-    const router = useRouter()
-    const [storylines, setStorylines] = useState([])
+	const router = useRouter()
+	const [storylines, setStorylines] = useState(null)
 
-    useEffect(() => {
-        /* when logged in show storylines, otherwise go to login page */
-        getAuth().onAuthStateChanged(function (user) {
-            if (user) {
-                getStorylineList().then(newStorylines => {
-                    setStorylines(newStorylines)
-                })
-            } else
-                router.push("/Login")
-        });
-    }, []);
+	useEffect(() => {
+		/* when logged in show storylines, otherwise go to login page */
+		getAuth().onAuthStateChanged(function(user) {
+			if (user) {
+				getStorylineList().then(newStorylines => {
+					setStorylines(newStorylines)
+				})
+			} else
+				router.push("/Login")
+		});
+	}, []);
 
-    return (
-        < div className='p-10' >
-            {
-                storylines.map((storyline, index) => (
-                    <div key={`${storyline}_${index}`} className='text-center m-5 align-middle'>
-                        <Link href={`/storyline/${storyline}`} id={`storyline_${index}`}>
-                            <CircleMission type={'gradient-' + (index % 3)}>
-                                {storyline.charAt(0).toUpperCase() + storyline.slice(1)}
-                            </CircleMission>
-                        </Link>
-                    </div>
-                ))
-            }
-        </div >
-    );
+	if (!storylines)
+		return <Loading />
+
+	return (
+		< div className='p-10' >
+			{
+				storylines.map((storyline, index) => (
+					<div key={`${storyline}_${index}`} className='text-center m-5 align-middle'>
+						<Link href={`/storyline/${storyline}`} id={`storyline_${index}`}>
+							<CircleMission type={'gradient-' + (index % 3)}>
+								{storyline.charAt(0).toUpperCase() + storyline.slice(1)}
+							</CircleMission>
+						</Link>
+					</div>
+				))
+			}
+		</div >
+	);
 }
