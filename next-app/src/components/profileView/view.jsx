7f7be0a11f1fd1477c74283976c508c8741917a7
marti-brocchi <marti.brocchi@gmail.com>
2024-02-26 14:28:19 +0100

    started working on profile refactoring

--- next-app/src/components/profileView/view.jsx
+++ next-app/src/components/profileView/view.jsx
@@ -4,14 +4,14 @@ import Button from "@/components/button/button";
 import { getAuth } from "firebase/auth";
 import { addFriendRequest, acceptFriendRequest, declineFriendRequest, getUserData, getAvatarByUsername } from "@/app/user_actions.js"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUsers, faBell, faCheck, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { faUsers, faBell, faCheck, faTimes, faMedal } from '@fortawesome/free-solid-svg-icons'
 import { app } from "@/firebase/index";
 import { useRouter } from "next/navigation";
 
 const auth = getAuth(app);
 
 export default function ProfileView({ email, avatar, badges, username, friends, friendRequests, score }) {
-    
+
     const router = useRouter();
     const [isOwner, setIsOwner] = useState(false);
     const [errorMessage, setErrorMessage] = useState(null);
@@ -85,117 +85,107 @@ export default function ProfileView({ email, avatar, badges, username, friends, 
 
 
     return (
-        <div className="flex flex-col items-center w-full">
-            <div className="flex w-8/12 justify-center">
-                <div className="w-10/12 max-w-10/12 min-h-[30vh] justify-center">
-                    <div className="flex flex-row p-4 justify-center">
-                        <div className="flex flex-col items-center justify-center">
-                            <img src={`/avatars/${avatar}.png`} alt={`avatar${avatar}`} className="rounded-full m-3 w-40 h-40 border-2 border-white" />
-
-                            {isOwner && <Button type='blue' href="/changeAvatar">Change Avatar</Button>}
-                        </div>
-
-                        <div className="ml-5 mt-auto mb-auto">
-                            <div className="flex flex-row items-center">
-
-                                <h1 className="text-5xl font-bold mr-8">{username}</h1>
-                                {isOwner && <Button type='blue' href="/changeUsername">Change Username</Button>}
-                                {isOwner && <Button type='blue' classNames="mx-4" onClick={() => {
-                                    auth.signOut();
-                                    router.push("/Login")
-                                }}>Logout</Button>}
-
-                            </div>
-                            {isOwner && <p className="text-xl mt-4">Email: {email}</p>}
-
-                            <p className="text-xl mt-4">Score: {score}</p>
-
-                            <div className="flex flex-row ">
-                                {badges.map((badge, index) => (
-                                    <img title={badge.replace(/_/g, " ")} key={index} src={`/badges/${badge}.png`} alt={`badge${badge}`} className="w-10 h-10 mr-2 mt-4" />
-                                ))}
-                            </div>
-                        </div>
+        <div className="flex flex-col justify-center items-center w-full">
+            <div className="flex flex-row justify-center">
+                <div className="flex flex-row p-4 justify-center">
+                    <div className="flex flex-col items-center justify-center">
+                        <img src={`/avatars/${avatar}.png`} alt={`avatar${avatar}`} className="rounded-full m-3 w-40 h-40 border-2 border-white" />
+                        {isOwner && <Button type='blue' href="/changeAvatar">Change Avatar</Button>}
                     </div>
 
-                    {
-                        isOwner ? null : (
-                            <div className="flex justify-center p-4 flex-col">
-                                {
-                                    status === 'friend' ? (<Button type='green'>Mutual friends</Button>) : null
-                                }
-                                {
-                                    status === 'request' ? (<Button type='green'>Request sent</Button>) : null
-                                }
-                                {
-                                    status === 'stranger' ? (<Button type='blue' onClick={handleAddFriend} id="add_friend">Add Friend</Button>) : null
-                                }
-                                {errorMessage && <div className="error-message">{errorMessage}</div>}
-                            </div>
-                        )
-                    }
-            </div>
-        </div>
+                    <div className="ml-5 mt-auto mb-auto">
+                        <div className="flex flex-row items-center">
 
-            <div className="flex w-7/12 justify-center">
-                <div className="w-1/2 max-w-1/2 p-3 min-h-[10vh]">
-                    <Button type="blue" href="/searchPlayers">Find Friends</Button>
-                    <h2 className="text-3xl font-bold m-auto my-4">
-                        <FontAwesomeIcon icon={faUsers} className="mr-2 max-w-[50px]" />
-                        Friends
-                    </h2>
-                    <div className="flex">
-                        {friendsList.length > 0 ? (
-                            <ul>
-                                {friendsList.map((friend, index) => (
-                                    <li key={`f_${index}_${friend}`} className="flex items-center mb-2 ">
-                                        <a href={`/profile/${friend}`} className="flex items-center hover:text-blue-500 hover:underline">
-                                            {avatars[friend] !== undefined && <img src={`/avatars/${avatars[friend]}.png`} alt={`avatar`} className="rounded-full w-16 h-16 mr-2" />}
-                                            <span className="text-xl px-2">{friend}</span>
-                                        </a>
-                                    </li>
-                                ))}
-                            </ul>
-                        ) : (
-                            <p>No friends found.</p>
+                            <h1 className="text-5xl font-bold mr-8">{username}</h1>
+                            {isOwner && <Button type='blue' href="/changeUsername">Change Username</Button>}
+                            {isOwner && <Button type='blue' classNames="mx-4" onClick={() => {
+                                auth.signOut();
+                                router.push("/Login")
+                            }}>Logout</Button>}
+
+                        </div>
+                        {isOwner && <p className="text-xl mt-4">Email: {email}</p>}
+                        <p className="text-xl mt-4">Score: {score}</p>
+
+                        {isOwner ? <></> : (
+                            <>
+                                {status === 'friend' ? (<Button classNames="my-4" type='green'>Mutual friends</Button>) : null}
+                                {status === 'request' ? (<Button classNames="my-4" type='green'>Request sent</Button>) : null}
+                                {status === 'stranger' ? (<Button classNames="my-4" type='blue' onClick={handleAddFriend} id="add_friend">Add Friend</Button>) : null}
+                                {errorMessage && <div className="error-message">{errorMessage}</div>}
+                            </>
                         )}
                     </div>
                 </div>
             </div>
 
-            <div className="flex w-7/12 justify-center">
+
+            <div className="flex flex-col p-3 space-y-4 w-full max-w-lg m-auto">
+
+                <h2 className="text-3xl font-bold m-auto">
+                    <FontAwesomeIcon icon={faMedal} className="mr-2 max-w-[50px]" />
+                    Badges
+                </h2>
+
+                <div className="flex flex-row flex-wrap pb-5">
+                    {badges.length != 0 ? badges.map((badge, index) => (
+                        <img title={badge.replace(/_/g, " ")} key={index} src={`/badges/${badge}.png`} alt={`badge${badge}`} className="w-10 h-10 mr-2 mt-4" />
+                    ))
+                        :
+                        isOwner ? <p>Complete missions to earn badges!</p> : <p>No badges found.</p>
+                    }
+                </div>
+
+                <h2 className="text-3xl font-bold m-auto">
+                    <FontAwesomeIcon icon={faUsers} className="mr-2 max-w-[50px]" />
+                    Friends
+                </h2>
+
+                {isOwner ? <Button classNames="flex max-w-sm m-auto justify-center" type="blue" href="/searchPlayers">Find Friends</Button> : <></>}
+
+                {friendsList.length > 0 ? (
+                    <ul>
+                        {friendsList.map((friend, index) => (
+                            <li key={`f_${index}_${friend}`} className="flex items-center mb-2 ">
+                                <a href={`/profile/${friend}`} className="flex items-center hover:text-blue-500 hover:underline">
+                                    {avatars[friend] !== undefined && <img src={`/avatars/${avatars[friend]}.png`} alt={`avatar`} className="rounded-full w-16 h-16 mr-2" />}
+                                    <span className="text-xl px-2">{friend}</span>
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                ) : (
+                    <p>No friends found.</p>
+                )}
+
                 {isOwner ?
                     (<>
-                        <div className="w-1/2 max-w-1/2 p-3 min-h-[10vh]">
-                            <h2 className="text-3xl font-bold mb-4">
-                                <FontAwesomeIcon icon={faBell} className="mr-2 max-w-[50px]" />
-                                Friend Requests
-                            </h2>
-                            <div className="flex">
-                                {friendRequestsList.length > 0 ? (
-                                    <ul>
-                                        {friendRequestsList.map((friend, index) => (
-                                            <li key={`fr_${index}_${friend}`} className="flex items-center justify-between mb-2 ">
-                                                <a href={`/profile/${friend}`} className="flex items-center hover:text-blue-500 hover:underline">
-                                                    {avatars[friend] !== undefined && <img src={`/avatars/${avatars[friend]}.png`} alt={`avatar`} className="rounded-full w-16 h-16 mr-2" />}
-                                                    <span className="text-xl px-2">{friend}</span>
-                                                </a>
-                                                <div className="ml-3">
-                                                    <Button classNames="mx-1" type="green" onClick={() => { handleAccept(friend) }}><FontAwesomeIcon icon={faCheck}/></Button>
-                                                    <Button classNames="mx-1" type="red" onClick={() => { handleDecline(friend) }}><FontAwesomeIcon icon={faTimes}/></Button>
-                                                </div>
-                                            </li>
-                                        ))}
-                                    </ul>
-                                ) : (
-                                    <p>No friend requests found.</p>
-                                )}
-                            </div>
-                        </div>
+                        <h2 className="text-3xl font-bold m-auto mb-4">
+                            <FontAwesomeIcon icon={faBell} className="mr-2 max-w-[50px]" />
+                            Friend Requests
+                        </h2>
+                        {friendRequestsList.length > 0 ? (
+                            <ul>
+                                {friendRequestsList.map((friend, index) => (
+                                    <li key={`fr_${index}_${friend}`} className="flex items-center justify-between mb-2 ">
+                                        <a href={`/profile/${friend}`} className="flex items-center hover:text-blue-500 hover:underline">
+                                            {avatars[friend] !== undefined && <img src={`/avatars/${avatars[friend]}.png`} alt={`avatar`} className="rounded-full w-16 h-16 mr-2" />}
+                                            <span className="text-xl px-2">{friend}</span>
+                                        </a>
+                                        <div className="ml-3">
+                                            <Button classNames="mx-1" type="green" onClick={() => { handleAccept(friend) }}><FontAwesomeIcon icon={faCheck} /></Button>
+                                            <Button classNames="mx-1" type="red" onClick={() => { handleDecline(friend) }}><FontAwesomeIcon icon={faTimes} /></Button>
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
+                        ) : (
+                            <p>No friend requests found.</p>
+                        )}
                     </>) :
-                    (null)
+                    <></>
                 }
             </div>
-        </div>
+        </div >
     );
 }
